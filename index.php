896592c4f8dc43283e601defb3df2433083938f5
pasha <pavle.prezent@gmail.com>
2020-04-24 22:26:36 +0300

    homework 11.1

--- index.php
+++ index.php
@@ -1,22 +1,4 @@
 <?php
-//echo $_SERVER['QUERY_STRING'];
-//switch ($_SERVER['QUERY_STRING']) {
-//    case '':
-//        require_once 'templates/index.html';
-//        break;
-//    case 'blog':
-//        require_once 'templates/blog-full.html';
-//        break;
-//    case 'services':
-//        require_once 'templates/services.html';
-//        break;
-//    case 'team':
-//        require_once 'templates/team.html';
-//        break;
-//    case 'contact_us':
-//        require_once 'templates/contact.html';
-//        break;
-//}
 require_once 'vendor/autoload.php';
 $a = new \App\Route();
 $a->route();
